b89c6143d307cb824924a93106be0adf5a7d75ae
Dvir <dvir019@gmail.com>
2019-11-12 20:19:52 +0200

    Add most of code for player A

---
+++ hw1q3.py
@@ -0,0 +1,42 @@
+def main():
+    # Get the cards of player A
+    print("Player A,")
+    print("Insert 1st card:")
+    a_first_card = input()
+    print("Insert 2nd card:")
+    a_second_card = int(input())
+    print("Insert 3rd card:")
+    a_third_card = input()
+
+    # Get the cards of player A
+    print("Player B,")
+    print("Insert 1st card:")
+    b_first_card = input()
+    print("Insert 2nd card:")
+    b_second_card = int(input())
+    print("Insert 3rd card:")
+    b_third_card = input()
+
+    # Get the weight of the first card of player A
+    a_first_card_weight = ord('A') - ord(a_first_card)
+    if ord('a') <= ord(a_first_card) <= ord('z'):
+        a_first_card_weight = ord('a') - ord(a_first_card)
+
+    # Get the weight of the second card of player A
+    units_digit = a_second_card % 10
+    a_second_card //= 10
+    tens_digit = a_second_card % 10
+    a_second_card //= 10
+    hundreds_digit = a_second_card % 10
+    a_second_card_weight = (tens_digit ** units_digit) // hundreds_digit
+
+    # Get the weight of the third card of player A
+    a_third_card_weight = 0
+    if a_third_card % 7 == 0:
+        a_third_card_weight = 17
+    elif a_third_card % 4 == 0:
+        a_third_card_weight = 6
+
+
+if __name__ == '__main__':
+    main()

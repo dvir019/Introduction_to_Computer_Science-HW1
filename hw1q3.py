7891375dba4334d029b621bc8f51b225c0c2b173
Dvir <dvir019@gmail.com>
2019-11-12 20:56:35 +0200

    Fix some bugs and add prints

--- hw1q3.py
+++ hw1q3.py
@@ -18,9 +18,10 @@ def main():
     b_third_card = input()
 
     # Calculate the weight of the first card of player A
-    a_first_card_weight = ord('A') - ord(a_first_card)
-    if ord('a') <= ord(a_first_card) <= ord('z'):
-        a_first_card_weight = ord('a') - ord(a_first_card)
+    ord_value = ord(a_first_card)
+    a_first_card_weight = ord_value - ord('A')  # Assume it's uppercase
+    if ord('a') <= ord_value <= ord('z'):  # Check if it's lowercase
+        a_first_card_weight =  ord_value-ord('a')
 
     # Calculate the weight of the second card of player A
     units_digit = a_second_card % 10
@@ -31,29 +32,21 @@ def main():
     a_second_card_weight = (tens_digit ** units_digit) // hundreds_digit
 
     # Calculate the weight of the third card of player A
+    ord_value = ord(a_third_card)
     a_third_card_weight = 0
-    if a_third_card % 7 == 0:
+    if ord_value % 7 == 0:
         a_third_card_weight = 17
-    elif a_third_card % 4 == 0:
+    elif ord_value % 4 == 0:
         a_third_card_weight = 6
 
     # Calculate the total cards weight of player A
     a_cards_total_weight = a_first_card_weight + a_second_card_weight + a_third_card_weight
 
-
-
-
-
-
-
-
-
-
-
     # Calculate the weight of the first card of player B
-    b_first_card_weight = ord('A') - ord(b_first_card)
-    if ord('a') <= ord(b_first_card) <= ord('z'):
-        b_first_card_weight = ord('a') - ord(b_first_card)
+    ord_value = ord(b_first_card)
+    b_first_card_weight = ord_value - ord('A')  # Assume it's uppercase
+    if ord('a') <= ord_value <= ord('z'):  # Check if it's lowercase
+        b_first_card_weight =  ord_value-ord('a')
 
     # Calculate the weight of the second card of player B
     units_digit = b_second_card % 10
@@ -64,17 +57,36 @@ def main():
     b_second_card_weight = (tens_digit ** units_digit) // hundreds_digit
 
     # Calculate the weight of the third card of player B
+    ord_value = ord(b_third_card)
     b_third_card_weight = 0
-    if b_third_card % 7 == 0:
+    if ord_value % 7 == 0:
         b_third_card_weight = 17
-    elif b_third_card % 4 == 0:
+    elif ord_value % 4 == 0:
         b_third_card_weight = 6
 
     # Calculate the total cards weight of player B
     b_cards_total_weight = b_first_card_weight + b_second_card_weight + b_third_card_weight
 
-    # Calculate the final result of the game
+    # Calculate the final result of the game -     It's a tie.     The winner is Player A!
+    game_result = "It's a tie."
+    if a_cards_total_weight > b_cards_total_weight:
+        game_result = "The winner is Player A!"
+    elif b_cards_total_weight > a_cards_total_weight:
+        game_result = "The winner is Player B!"
+    elif a_second_card_weight > b_second_card_weight:
+        game_result = "The winner is Player A!"
+    elif b_second_card_weight > a_second_card_weight:
+        game_result = "The winner is Player B!"
 
+    # Print the results
+    print(f"Score A - total:{a_cards_total_weight}, card2:{a_second_card_weight}")
+    print(f"Score B - total:{b_cards_total_weight}, card2:{b_second_card_weight}")
+    print(game_result)
+
+
+    print("\n\n********************")
+    print(f"A: {a_first_card_weight}, {a_second_card_weight}, {a_third_card_weight}")
+    print(f"B: {b_first_card_weight}, {b_second_card_weight}, {b_third_card_weight}")
 
 
 if __name__ == '__main__':

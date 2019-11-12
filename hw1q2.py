b889d290559e1cf5e13bb3a161defebd0883e93e
Dvir <dvir019@gmail.com>
2019-11-12 18:55:16 +0200

    Finish almost everything, only documentation to the main id missing

--- hw1q2.py
+++ hw1q2.py
@@ -4,36 +4,50 @@ def main():
     supervisor = "s"
     manager = "m"
 
-    # Set the days
-    sunday = "sun"
-    monday = "mon"
-    tuesday = "tue"
-    wednesday = "wed"
-    thursday = "thu"
+    # Set the weekend days
+    # *** No need to save the other days, because I will check if it's weekend,
+    #     and if it's not, than it's weekday.
     friday = "fri"
     saturday = "sat"
 
     # Set the salaries per hour
-    # * Weekday = sunday until thursday
-    # * Weekend = friday until saturday
+    # *** Weekday = sunday until thursday
+    # *** Weekend = friday until saturday
     weekday_up_to_8_hours = 30
     weekday_more_than_8_hours = 35
     weekend_up_to_8_hours = 42
     weekend_more_than_8_hours = 49
 
     # Get the employee type
-    print("Menu:\nr: Regular\ns: Supervisor\nm: Manager")
+    print(f"Menu:\n{regular}: Regular\n{supervisor}: Supervisor\n{manager}: Manager")
     employee_type = input()
 
     # Get the work day
-    print("Enter work day:")
+    print("\nEnter work day:")
     work_day = input()
 
     # Get the work hours
     print("Enter work hours:")
-    work_hours = input()
+    work_hours = int(input())
 
     # Calculate the base salaries
+    weekday_salary = weekday_up_to_8_hours * min(work_hours, 8) + \
+                     weekday_more_than_8_hours * max(work_hours - 8, 0)
+    weekend_salary = weekend_up_to_8_hours * min(work_hours, 8) + \
+                     weekend_more_than_8_hours * max(work_hours - 8, 0)
+
+    # Check if it's weekend
+    is_weekend = work_day == friday or work_day == saturday
+
+    # Calculate the total salary
+    total_salary = weekend_salary * int(is_weekend) + \
+                   weekday_salary * int(not is_weekend)
+    total_salary *= 1 * int(employee_type == regular) + \
+                    1.2 * int(employee_type == supervisor) + \
+                    1.5 * int(employee_type == manager)
+
+    # Print the result
+    print(f"The daily salary is {total_salary:.1f}")
 
 
 if __name__ == '__main__':

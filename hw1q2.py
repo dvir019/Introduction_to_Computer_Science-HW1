cd44c93c7205faa4f6106a4eb765926182423852
Dvir <dvir019@gmail.com>
2019-11-12 18:07:37 +0200

    Set the more base variables

--- hw1q2.py
+++ hw1q2.py
@@ -13,6 +13,14 @@ def main():
     friday = "fri"
     saturday = "sat"
 
+    # Set the salaries per hour
+    # * Weekday = sunday until thursday
+    # * Weekend = friday until saturday
+    weekday_up_to_8_hours = 30
+    weekday_more_than_8_hours = 35
+    weekend_up_to_8_hours = 42
+    weekend_more_than_8_hours = 49
+
     # Get the employee type
     print("Menu:\nr: Regular\ns: Supervisor\nm: Manager")
     employee_type = input()
@@ -24,6 +32,8 @@ def main():
     # Get the work hours
     print("Enter work hours:")
     work_hours = input()
+
+    # Calculate the base salaries
 
 
 if __name__ == '__main__':
